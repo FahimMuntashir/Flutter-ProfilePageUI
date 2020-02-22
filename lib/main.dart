91fc77c8d21c8a6d43fed03fe69454dba2d0a838
FahimMuntashir <fahim.tech.me@gmail.com>
2020-02-23 00:29:07 +0600

    adding emain card

--- lib/main.dart
+++ lib/main.dart
@@ -33,15 +33,14 @@ class Home extends StatelessWidget {
                 fontFamily: 'Pacifico',
               ),
             ),
-            Text('Passionate Developer',
+            Text(
+              'Passionate Developer',
               style: TextStyle(
-                fontSize: 30.0,
-                color: Colors.blueGrey[200],
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Source Sans Pro'
-
-              ),
+                  fontSize: 30.0,
+                  color: Colors.blueGrey[200],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro'),
             ),
             SizedBox(
               height: 20,
@@ -55,25 +54,42 @@ class Home extends StatelessWidget {
               padding: EdgeInsets.all(10.0),
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
               child: Row(
-              children: <Widget>[
-                Icon(
-                  Icons.phone,
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+8348388348',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
                   color: Colors.teal,
                 ),
-                SizedBox(
-                  width: 10.0,
+                title: Text(
+                  'fahim.tech.me@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Pro',
+                  ),
                 ),
-                Text(
-                  '+8348388348',
-                 style: TextStyle(
-                   color:Colors.teal,
-                   fontFamily: 'Source Sans Pro',
-                   fontSize: 20.0,
-                 ),
-                ),
-              ],
               ),
-
             ),
           ],
         ),
